91304d5b1d5b375fdb40f8ea57d3d58ceeebf4a8
anjunshao <shaoanjun@hotmail.com>
2019-05-20 21:02:59 -0400

    Create Body.java

---
+++ Body.java
@@ -0,0 +1,10 @@
+public class Body{
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+
+	public Body(Body b);
+}
