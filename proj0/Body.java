440ed71ab44733f82cef66ac72126c83f9c6c835
anjunshao <shaoanjun@hotmail.com>
2019-05-21 22:28:53 -0400

    added method for r and force, force X, force Y

--- proj0/Body.java
+++ proj0/Body.java
@@ -24,7 +24,31 @@ public class Body{
 		mass = b.mass;
 		imgFileName = b.imgFileName;
 	}
-		
-		
+	  public double calcDistance(Body b){
+		double dx = xxPos-b.xxPos;
+		double dy = yyPos - b.yyPos;
+		double r = Math.sqrt(dx*dx + dy*dy);
+		return r;
+	}
+	public double calcForceExertedBy(Body b){
+		double G = 6.67e-11;
+		double r = calcDistance(b);
+		double Force = G*mass*b.mass/r/r;
+		return Force;
+	}
+	public double calcForceExertedByX(Body b){
+		double dx = b.xxPos-xxPos;
+		double r = calcDistance(b);
+		double force = calcForceExertedBy(b);
+		double ForceByX = force*dx/r;
+		return ForceByX;
+	}
+	public double calcForceExertedByY(Body b){
+		double dy = b.yyPos-yyPos;
+		double r = calcDistance(b);
+		double force = calcForceExertedBy( b);
+		double ForceByY = force*dy/r;
+		return ForceByY;
+	}
 
 	}
